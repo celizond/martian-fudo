b953a260439293a3d2ece1c39a9db72081e5cafc
celizond <elizondo.celular@gmail.com>
2025-07-18 14:33:46 -0300

    aSide

--- src/components/aSide/ASide.tsx
+++ src/components/aSide/ASide.tsx
@@ -1,12 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import './ASide.scss';
+import { useContext } from 'react';
+import { AuthContext } from '../../features/auth/context';
 
 export const ASide = () => {
+
+  const navigate = useNavigate();
+  const { logout } = useContext(AuthContext);
+
+  const onGoHome = () => {
+    window.location.reload()
+    navigate('/home')
+  }
+
   return (
     <aside>
       <ul>
-        <li>Mis posts</li>
-        <li>Feed</li>
-        <li>Salir</li>
+        {/*  <li onClick={onGoHome}>&#128100; MIS POSTS</li> */}
+        <li onClick={onGoHome}>&#127968; FEED</li>
+        <li onClick={logout}>&#128162; SALIR</li>
       </ul>
     </aside>
   )
